895e41bd2da25a509a8577e918d137f253e11fec
sskkp1025 <lion8961@gmail.com>
2026-09-05 05:12:42 +0900

    MainActivity.kt 업데이트

--- app/src/main/java/com/athenaz/app/MainActivity.kt
+++ app/src/main/java/com/athenaz/app/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val terminalWebView = findViewById<WebView>(R.id.terminalWebView)
 
+        // 터미널 화면(ttyd)을 웹뷰에 띄우기 위해 필수적인 자바스크립트 허용 세팅
         terminalWebView.apply {
             settings.javaScriptEnabled = true
             settings.domStorageEnabled = true
@@ -26,17 +27,21 @@ class MainActivity : AppCompatActivity() {
             webChromeClient = WebChromeClient()
         }
 
+        // 하단 탭을 눌렀을 때의 동작 설정
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_tab1 -> {
+                    // 1번째 탭: 터미널 숨김
                     terminalWebView.visibility = View.GONE
                     true
                 }
                 R.id.navigation_tab2 -> {
+                    // 2번째 탭: 터미널 숨김
                     terminalWebView.visibility = View.GONE
                     true
                 }
                 R.id.navigation_tab3 -> {
+                    // 3번째 탭: 터미널 화면 표시 및 AWS 서버 터미널(7681 포트) 접속
                     terminalWebView.visibility = View.VISIBLE
                     terminalWebView.loadUrl("http://45.76.195.208:7681")
                     true
